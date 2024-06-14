07b8cbc022a8a23ac513b262ee8b7583e604798b
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-06-14 16:13:42 +0000

    support redirect

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
-import { Hono } from 'hono';
+import { Context, Hono } from 'hono';
 
 const app = new Hono();
 
 const baseUrl = 'https://ame-x.net';
 
-app.all('*', (c) => c.text('Test'));
+app.all('*', async (c: Context) => {
+	const urlObj = new URL(c.req.url);
+	const pathNameAndQuery = `${urlObj.pathname}${urlObj.search}`;
+	const method = c.req.method;
+	const headers = c.req.raw.headers;
+	const body = c.req.raw.body;
+	const buildedUrl = `${baseUrl}${pathNameAndQuery}`;
+	const response = await fetch(buildedUrl, {
+		method,
+		headers,
+		body,
+	});
+
+	const redirectDetect = response.headers.get('Location');
+	if (redirectDetect && redirectDetect.includes(baseUrl)) {
+		response.headers.set('Location', redirectDetect.replace(baseUrl, ''));
+	}
+
+	return response as unknown as Response;
+});
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		const urlObj = new URL(request.url);
+		console.log(`[LOG]:"${request.method}":"${new Date().getTime()}":"${urlObj.pathname}${urlObj.search}"`);
 		return app.fetch(request, env, ctx);
 	},
 } satisfies ExportedHandler<Env>;
